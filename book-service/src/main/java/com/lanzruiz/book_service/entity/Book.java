0034bd11d7a27ade0d88b1a46ff94b755991c878
Lanz Ruiz <lanz.primeitph@gmail.com>
2026-06-13 17:44:20 +0800

    setup entity

---
+++ book-service/src/main/java/com/lanzruiz/book_service/entity/Book.java
@@ -0,0 +1,30 @@
+package com.lanzruiz.book_service.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="book")
+public class Book {
+	
+   @Id
+   @GeneratedValue(strategy=GenerationType.IDENTITY)
+   private Long id;
+   
+   @Column(name="title", nullable=false)
+   private String title;
+   private String author;
+   private int isbn;
+}
